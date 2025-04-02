aadb8073f34335666c8d0ef2c9778af3931bca4d
Bram <bram@fluet.dev>
2025-04-02 23:26:43 +0200

    working base application

--- src/main/java/com/brampeerdeman/GereedschapsManager/controller/GereedschapController.java
+++ src/main/java/com/brampeerdeman/GereedschapsManager/controller/GereedschapController.java
@@ -1,16 +1,15 @@
 package com.brampeerdeman.GereedschapsManager.controller;
 
+import com.brampeerdeman.GereedschapsManager.exception.GereedschapNotFoundException;
 import com.brampeerdeman.GereedschapsManager.model.Gereedschap;
 import com.brampeerdeman.GereedschapsManager.repository.GereedschapRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class GereedschapController
 {
     @Autowired
@@ -26,5 +25,35 @@ public class GereedschapController
     List<Gereedschap> getAllGereedschappen()
     {
         return gereedschapRepository.findAll();
+    }
+
+    @GetMapping("/gereedschap/{id}")
+    Gereedschap getGereedschapById(@PathVariable Long id)
+    {
+        return gereedschapRepository.findById(id)
+                .orElseThrow(() -> new GereedschapNotFoundException(id));
+    }
+
+    @PutMapping("/gereedschap/{id}")
+    Gereedschap updateGereedschap(@RequestBody Gereedschap newGereedschap, @PathVariable Long id)
+    {
+        return gereedschapRepository.findById(id)
+                .map(gereedschap -> {
+                    gereedschap.setName(newGereedschap.getName());
+                    gereedschap.setLocation(newGereedschap.getLocation());
+                    return gereedschapRepository.save(gereedschap);
+                }).orElseThrow(() -> new GereedschapNotFoundException(id));
+    }
+
+    @DeleteMapping("/gereedschap/{id}")
+    String deleteGereedschap(@PathVariable Long id)
+    {
+        if(!gereedschapRepository.existsById(id))
+        {
+            throw new GereedschapNotFoundException(id);
+        }
+        gereedschapRepository.deleteById(id);
+
+        return "Gereedschap with id " + id + " was deleted successfully";
     }
 }
