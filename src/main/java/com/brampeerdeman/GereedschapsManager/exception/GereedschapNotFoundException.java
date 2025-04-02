cada49318cd2008bee42833830eff08bd7247204
Bram Peerdeman <114592565+BramPeerdeman@users.noreply.github.com>
2025-04-02 23:32:11 +0200

    Revert "GM-1"

--- src/main/java/com/brampeerdeman/GereedschapsManager/exception/GereedschapNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.brampeerdeman.GereedschapsManager.exception;
-
-public class GereedschapNotFoundException extends RuntimeException
-{
-    public GereedschapNotFoundException(Long id)
-    {
-        super("User with id " + id + " not found");
-    }
-}
--- src/main/java/com/brampeerdeman/GereedschapsManager/repository/GereedschapRepository.java
+++
@@ -1,9 +0,0 @@
-package com.brampeerdeman.GereedschapsManager.repository;
-
-import com.brampeerdeman.GereedschapsManager.model.Gereedschap;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GereedschapRepository extends JpaRepository<Gereedschap, Long>
-{
-
-}
